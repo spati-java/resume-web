b207ce52c00788b128257e900353e45abd27d637
unknown <sourav.pati2007@gmail.com>
2015-07-01 15:33:01 -0700

    added animation code

---
+++ WebContent/resources/js/animation.js
@@ -0,0 +1,21 @@
+
+
+  $(function() {
+    var state = true;
+    $( "#button" ).click(function() {
+      if ( state ) {
+        $( "#effect" ).animate({
+          backgroundColor: "#aa0000",
+          color: "#fff",
+          width: 500
+        }, 1000 );
+      } else {
+        $( "#effect" ).animate({
+          backgroundColor: "#fff",
+          color: "#000",
+          width: 240
+        }, 1000 );
+      }
+      state = !state;
+    });
+  });
